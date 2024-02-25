5b247de369d7afcf7a782f7f2a4aa5e6d439738d
Arman Nita <161221335+armansite@users.noreply.github.com>
2024-02-25 14:54:47 +0100

    BrowserConfig.cs

--- src/Browser/BrowserConfig.cs
+++ src/Browser/BrowserConfig.cs
@@ -9,16 +9,16 @@ namespace SharpBrowser.Browser {
 	internal static class BrowserConfig {
 
 
-		public static string Branding = "SharpBrowser";
+		public static string Branding = "StellarSurf";
 		public static string AcceptLanguage = "en-US,en;q=0.9";
 		public static string UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.102 Safari/537.36 /CefSharp Browser" + Cef.CefSharpVersion; // UserAgent to fix issue with Google account authentication		
-		public static string HomepageURL = "https://www.google.com";
-		public static string NewTabURL = "about:blank";
-		public static string InternalURL = "sharpbrowser";
-		public static string DownloadsURL = "sharpbrowser://storage/downloads.html";
-		public static string FileNotFoundURL = "sharpbrowser://storage/errors/notFound.html";
-		public static string CannotConnectURL = "sharpbrowser://storage/errors/cannotConnect.html";
-		public static string SearchURL = "https://www.google.com/search?q=";
+		public static string HomepageURL = "https://armansite.neocities.org/StartPage";
+		public static string NewTabURL = "https://armansite.neocities.org/StartPage";
+		public static string InternalURL = "StellarSurf";
+		public static string DownloadsURL = "StellarSurf://storage/downloads.html";
+		public static string FileNotFoundURL = "StellarSurf://storage/errors/notFound.html";
+		public static string CannotConnectURL = "StellarSurf://storage/errors/cannotConnect.html";
+		public static string SearchURL = "https://duckduckgo.com/?q=";
 
 		public static bool WebSecurity = true;
 		public static bool CrossDomainSecurity = true;
